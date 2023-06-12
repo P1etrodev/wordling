7bf96f4b585c662ffd5dc206dd1a701045fac8d9
Sylph1de <Francodavidpietrokovsky@gmail.com>
2023-06-12 17:27:55 -0300

    fixed relative routes

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from json import load
 from tools import play_wordle
+import sys
+from os.path import dirname, join
 
-with open('./config.json', 'r', encoding='utf-8') as f:
+if getattr(sys, 'frozen', False):
+    config_file = join(dirname(sys.executable), 'config.json')
+else:
+    config_file = join(dirname(__file__), 'config.json')
+
+with open(config_file, 'r', encoding='utf-8') as f:
     config = load(f)
 
 if __name__ == '__main__':
-    play_wordle('Wordling', config.get('min_length'), config.get('max_length'))
+    play_wordle('Wordling', min_len=config.get('min_length'), max_len=config.get('max_length'))
